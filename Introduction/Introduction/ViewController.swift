41f12508b4181341464fedeb3ab43b3429519301
RakibRary <60999379+RakibRary@users.noreply.github.com>
2023-03-20 11:43:00 -0400

    Add files via upload

---
+++ Introduction/Introduction/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Introduction
+//
+//  Created by Rakib Rary on 2/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func information(_ sender: Any) {
+    }
+    
+    
+    @IBAction func Gallery(_ sender: Any) {
+    }
+    
+    
+    @IBAction func M_Comic(_ sender: Any) {
+    }
+    
+    
+}
+
